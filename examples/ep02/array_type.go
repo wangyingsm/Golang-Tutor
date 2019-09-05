c1683f74b2415671946828441e1d61794215f503
wangy <yingge_gzb@qq.com>
2019-09-05 18:43:20 +0800

    ep02 init

--- examples/ep02/array_type.go
+++ examples/ep02/array_type.go
@@ -1,11 +1,12 @@
 package main
 
 func main() {
-	aArray := [5]int{0, 1, 2, 3, 4}
+	aArray := [5]int{0, 1, 2, 3, 4} //aArray is [5]int type
 	// fmt.Println(sum(aArray)) 编译错误
 	_ = aArray
 }
 
+// sum require a [6]int type argument
 func sum(a [6]int) int {
 	result := 0
 	for _, v := range a {
